b6ff609d8830d0433b4bc3371e4b6b2a6c29dea9
seven <xqdwmq@gmail.com>
2026-09-14 01:40:41 +0800

    chore(release): prepare v1.2.3

--- apps/server/lib/src/services/app_config.dart
+++ apps/server/lib/src/services/app_config.dart
@@ -8,7 +8,7 @@ final class AppConfig {
   bool get privateAccounts => get('GOMOKU_AUTH_MODE', 'email') == 'private';
   String get version => get(
     'GOMOKU_VERSION',
-    const String.fromEnvironment('GOMOKU_VERSION', defaultValue: '1.2.2'),
+    const String.fromEnvironment('GOMOKU_VERSION', defaultValue: '1.2.3'),
   );
   String get commit => get(
     'GOMOKU_COMMIT',
