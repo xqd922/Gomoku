b7167dd82e0c6a8b12db2b70e38b9b2aaede882f
seven <xqdwmq@gmail.com>
2026-09-19 23:52:05 +0800

    chore(release): prepare v1.4.0

--- apps/server/lib/src/services/app_config.dart
+++ apps/server/lib/src/services/app_config.dart
@@ -7,7 +7,7 @@ final class AppConfig {
 
   String get version => get(
     'GOMOKU_VERSION',
-    const String.fromEnvironment('GOMOKU_VERSION', defaultValue: '1.3.0'),
+    const String.fromEnvironment('GOMOKU_VERSION', defaultValue: '1.4.0'),
   );
   String get commit => get(
     'GOMOKU_COMMIT',
